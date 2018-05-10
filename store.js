c5ffad2978002e46b4532cca067ee9fc8208982d
Yasminabdallah <yasminabdalllahgaberm@gmail.com>
2018-05-10 18:00:44 +0200

    add add and clear functions

--- store.js
+++ store.js
@@ -4,32 +4,43 @@ var fs = require('fs');
 var action = process.argv.slice(2)[0];
 var argsone= process.argv.slice(2)[1];
 var argstwo=process.argv.slice(2)[2];
+var fileName='dictionary.txt';
 
-
-if (action =="add")
+console.log(action+argsone+argstwo)
+if (action =="add" && argsone && argstwo)
   {
     console.log( action +" "+ argsone +" "+argstwo);
-    fs.appendFile('dictionary.txt', 'Hello content!', function (err) {
+    var newobj= "key:"+ argsone+','+"value :"+ argstwo;
+   
+    fs.appendFileSync(fileName, '\r\n' + newobj, 'utf8', function (err) {
       if (err) throw err;
       console.log('Saved!');
     });
+   
+  
+
+    
+
   }
+  
+
 else if(action=="list")
   {
     console.log("no argument nedded");
   } 
-else if (action=="get")
+else if (action=="get" && argsone)
   {
     console.log( action +" "+ argsone );
   } 
-  else if (action =="remove"){
+  else if (action =="remove" &&argsone){
     console.log( action +" "+ argsone );
   }
   else if (action=="clear"){
-    console.log( "clear statment");
+    
+    fs.writeFile(fileName, '', function(){console.log('done')})
   }
   else {
-    console.log( "invalid ");
+    console.log( "invalid  operation !");
   }
 
 
