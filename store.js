fbecfb06b980580a7126b4db12e7f423b1ae850d
Yasminabdallah <yasminabdalllahgaberm@gmail.com>
2018-05-10 21:21:01 +0200

    add remove function

--- store.js
+++ store.js
@@ -4,72 +4,96 @@ var readline = require('readline');
 var LineByLineReader = require('line-by-line');
 
 var action = process.argv.slice(2)[0];
-var argsone= process.argv.slice(2)[1];
-var argstwo=process.argv.slice(2)[2];
-var fileName='dictionary.txt';
+var argsone = process.argv.slice(2)[1];
+var argstwo = process.argv.slice(2)[2];
+var fileName = 'dictionary.txt';
 
-if (action =="add" && argsone && argstwo)
-  {
-    console.log( action +" "+ argsone +" "+argstwo);
-    var newobj= "key:"+ argsone+','+"value:"+ argstwo;
-   
-    fs.appendFileSync(fileName, '\r\n' + newobj, 'utf8', function (err) {
-      if (err) throw err;
-      console.log('Saved!');
-    });  
 
-  }
-  
+if (action == "add" && argsone && argstwo) {
+  console.log(action + " " + argsone + " " + argstwo);
+  var newobj = "key:" + argsone + ',' + "value:" + argstwo;
 
-else if(action=="list")
-  {
-    fs.readFile(fileName, function(err, items) {
-      if (err) throw err;
-     
-        console.log("dictionary contents: " + items);
-         
+  fs.appendFileSync(fileName, '\r\n' + newobj, 'utf8', function (err) {
+    if (err) throw err;
+    console.log('Saved!');
   });
 
-  } 
- else if (action=="get" && argsone){
+}
 
-  var found =false;
+
+else if (action == "list") {
+  fs.readFile(fileName, function (err, items) {
+    if (err) throw err;
+
+    console.log("dictionary contents: " + items);
+
+  });
+
+}
+else if (action == "get" && argsone) {
+
+  let found = false;
   var lineReader = readline.createInterface({
     input: require('fs').createReadStream(fileName)
   });
-  
+
   lineReader.on('line', function (line) {
     var buf = Buffer.from(line);
-    
-    if (buf.indexOf('key:'+argsone)>-1){
+    let result = buf.indexOf('key:' + argsone);
+    if (result > -1) {
       console.log(line);
-      found=true;
+      found = true;
+
     }
-    
-     
-    
-    
+
+
   });
-  if (!found){
-    console.log("key not found");
-  }
 
 
- 
-    
-  } 
+}
 
-  else if (action =="remove" &&argsone){
-    console.log( action +" "+ argsone );
-  }
-  
-  else if (action=="clear"){
-    
-    fs.writeFile(fileName, '', function(){console.log('done')})
-  }
-  else {
-    console.log( "invalid  operation !");
-  }
+else if (action == "remove" && argsone) {
+
+
+  let linenum = 0;
+  let currentnumber = 0;
+  var lineReader = readline.createInterface({
+    input: require('fs').createReadStream(fileName)
+  });
+
+  lineReader.on('line', function (line) {
+    var buf = Buffer.from(line);
+    linenum++;
+
+    if (buf.indexOf('key:' + argsone) > -1) {
+      currentnumber = linenum;
+      fs.readFile(fileName, 'utf8', function (err, data) {
+        if (err) {
+          
+          console.log(err)
+        }
+        var linesExceptmatched = data.split('\n').slice(currentnumber).join('\n');
+        fs.writeFile(fileName, linesExceptmatched,function(err){
+          if(err){
+            console.log(err)
+          }
+        });
+       
+      });
+     
+    }
+  });
+
+
+}
+
+else if (action == "clear") {
+
+  fs.writeFile(fileName, '', function () { console.log('done') })
+}
+else {
+  console.log("invalid  operation !");
+}
 
 
 
